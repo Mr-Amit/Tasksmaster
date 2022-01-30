f5d9b316a60fefc9f31fa1fab6ef989763b470bd
Mr-Amit <amit.kumar221099@gmail.com>
2022-01-31 03:12:31 +0530

    validators and alerts

--- src/Components/SubmitModal.jsx
+++ src/Components/SubmitModal.jsx
@@ -1,38 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import '../Stylesheets/modal.css';
+import validator from 'validator'
 
 function SubmitModal( { setModal, data } ) {
 
     const [submitString, setSubmit] = useState('SUBMIT')
+    const [emailError, setEmailError] = useState('Enter a valid Email')
     const [email, setEmail] = useState('')
+
+    const validateEmail = (e) => {
+      var emailget = e.target.value
+      setEmail(emailget)
+      if (validator.isEmail(emailget)) {
+        setEmailError('')
+        
+      } else {
+        setEmailError('Enter a valid Email')
+      }
+    }
 
     const handleMailChange = event => {
       setEmail(event.target.value)
+      
     };
 
     const handleSubmit = (event) => {
-      data.email = email;
-      setSubmit("SUBSCRIBED")
-      // console.log(data);
+      if(emailError !== ''){
+        if(email === ''){
+          alert("Email Field is Empty")
+        } else {
 
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        
-        body:  JSON.stringify(data)
-      };
-      fetch('http://127.0.0.1:8000/', requestOptions)
-          .then(response => console.log("IN REACT" + response));
+          alert(email + " is not a valid email address")
+        }
+      } else {
+        data.email = email;
+        setSubmit("SUBSCRIBED")
+        // console.log(data);
+
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
           
-
+          body:  JSON.stringify(data)
+        };
+        fetch('http://127.0.0.1:8000/', requestOptions)
+            .then(response => console.log("IN REACT" + response));
+      }
+      
     }
 
     let displayText;
     if(data.type === "NewsForm"){
-      displayText = "You want News on " + (data.newsAbout || 'Anything(Default)') + " at " +  (data.wordLimit || '50(Default)') + " word limit?"
+      displayText = "Subscribe to News on " + (data.newsAbout || 'Anything(Default)') + " at " +  (data.wordLimit || '50(Default)') + " word limit?"
     }
     else if(data.type === "NasaForm"){
-      displayText = "Get daily Deep Space Images by Nasa?"
+      displayText = "Subscribe to daily Deep Space Images by Nasa?"
     }
     else if(data.type === "AstrologyForm"){
       displayText = "Get Daily Astrology For " + (data.zodiac) + "."
@@ -54,7 +76,9 @@ function SubmitModal( { setModal, data } ) {
             <div className='modal-email-submit'>
               <div className='modal-email'>
                 <label className="form-fields modal-fields" htmlFor="mail">E-Mail</label>
-                <input onChange={handleMailChange} className="form-fields modal-fields" type="text" id="mail" name="mail"/>
+                <input onChange={(e) => validateEmail(e)} className="form-fields modal-fields" type="email" value={email} id="mail" name="mail"/>
+                
+                <span className='span-modal'> {emailError} </span>
               </div>           
               
               <div>
